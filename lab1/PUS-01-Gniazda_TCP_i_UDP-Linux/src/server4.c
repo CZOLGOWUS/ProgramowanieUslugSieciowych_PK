2f01dffc320aea8c77dc1dc959a071eaf8f39d5c
CKTA00 <przekozuch@gmail.com>
2022-03-07 00:03:44 +0100

    Small cleanup

--- lab1/PUS-01-Gniazda_TCP_i_UDP-Linux/src/server4.c
+++ lab1/PUS-01-Gniazda_TCP_i_UDP-Linux/src/server4.c
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
 
     int main_sd;                // Deskryptor gniazda glownego.
     int client_sd[MAX_CLIENTS]; // Deksryptory klientow.
-    int retval;
+    int retval;                 
     int max_sd;                 // Maks. deskryptor (potrzebne funkcji select)
 
     /* Gniazdowe struktury adresowe (dla klienta i serwera): */
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
 
     /* Wyzerowanie struktury adresowej serwera: */
     memset(&server_addr, 0, sizeof(server_addr));
-    //initialise all client_socket[] to 0 so not checked 
+    // inicjalizacja listy klientow (0 = brak clienta)
     for (int i = 0; i < MAX_CLIENTS; i++)  
     {  
         client_sd[i] = 0;
@@ -75,53 +75,47 @@ int main(int argc, char** argv) {
     /* Rozmiar struktury adresowej serwera w bajtach: */
     server_addr_len                 =       sizeof(server_addr);
 
-    /* Powiazanie "nazwy" (adresu IP i numeru portu) z gniazdem: */
+    // Powiazanie "nazwy" (adresu IP i numeru portu) z gniazdem
     if (bind(main_sd, (struct sockaddr*) &server_addr, server_addr_len) == -1) {
         perror("bind()");
         exit(EXIT_FAILURE);
     }
 
-     //try to specify maximum of 3 pending connections for the master socket 
+    // Rozpocznij nasluch na glownym gniezdzie (deskryptorze)
     if (listen(main_sd, 3) < 0)  
     {  
         perror("listen");  
         exit(EXIT_FAILURE);  
     }  
-         
-    //accept the incoming connection 
-    puts("Waiting for connections ...");  
 
-    
+    printf("Waiting for connections ...");  
 
-    int current_sd; //temp
-    int activity;
-    int new_sd;
+    int current_sd;     // deskryptor, kory akurat nas ionteresuje w danym momencie petli
+    int activity;       // aktywnosc na porcie glownym
+    int new_sd;         // nowy deskryptor nowego polonczenia
     while(1)
     {
-        //clear the socket set 
         FD_ZERO(&descriptors_set);  
         
-        //add master socket to set 
+        //dodaj glowne gniazdo
         FD_SET(main_sd, &descriptors_set);  
         max_sd = main_sd;  
                 
-        //add child sockets to set 
         for (int i = 0 ; i < MAX_CLIENTS ; i++)  
         {  
-            //socket descriptor 
             current_sd = client_sd[i];  
 
-            //if valid socket descriptor then add to read list 
+            //dodaj deskryptor jesli jest to juz polonczony klient
             if(current_sd > 0)  
                 FD_SET( current_sd , &descriptors_set);
                     
-            //highest file descriptor number, need it for the select function 
+            //ustawianie nawyszego deskryptora, ktory jest potrzebny funkcji select
             if(current_sd > max_sd)  
                 max_sd = current_sd;  
         }  
         
 
-        //wait for an activity on one of the sockets
+        //zaczekaj na nowa aktywnosc na glownym gniezdzie
         activity = select( max_sd + 1 , &descriptors_set , NULL , NULL , NULL);  
         
         if ((activity < 0) && (errno!=EINTR))  
@@ -129,21 +123,20 @@ int main(int argc, char** argv) {
             printf("select() error");  
         }  
                 
-        
+        //nowe poloczenie
         if (FD_ISSET(main_sd, &descriptors_set))  
         {  
-            //Nowe poloczenie
-            if ((new_sd = accept(main_sd, 
-                    (struct sockaddr *)&client_addr, (socklen_t*)&client_addr_len))<0)  
+            //zaakceptuj je
+            if ((new_sd = accept(main_sd, (struct sockaddr *)&client_addr, (socklen_t*)&client_addr_len))<0)  
             {  
                 perror("accept() error");  
                 exit(EXIT_FAILURE);  
             }  
                 
-            //inform user of socket number - used in send and receive commands 
+            //wyswietl info o nowym kilencie
             printf("Nowe poloczenie %d\n  ip: %s\n  port : %d\n\n" , new_sd , inet_ntoa(client_addr.sin_addr) , ntohs(client_addr.sin_port)); 
                     
-            //dodaj nowego klienta
+            //dodaj nowego klienta do listy
             for (int i = 0; i < MAX_CLIENTS; i++)  
             {  
                 if( client_sd[i] == 0 )  
@@ -154,42 +147,36 @@ int main(int argc, char** argv) {
             }  
         }  
                 
-        //else its some IO operation on some other socket
         for (int i = 0; i < MAX_CLIENTS; i++)  
         {  
             current_sd = client_sd[i];  
             if (FD_ISSET(current_sd, &descriptors_set))  
             {  
-                //Check if it was for closing , and also read the incoming message 
                 if ((retval = read(current_sd, buff, BUFF_SIZE)) == 0)  
                 {  
-                    //Somebody disconnected , get his details and print 
-                    getpeername(current_sd , (struct sockaddr*)&client_addr , \
-                        (socklen_t*)&client_addr_len);  
-                    printf("Klient zamyka polonczenie\n  ip: %s\n  port: %d\n\n" , 
-                            inet_ntoa(client_addr.sin_addr) , ntohs(client_addr.sin_port));  
-                            
-                    close(current_sd);  
+                    //rozlonczenie
+                    getpeername(current_sd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);  
+                    printf("Klient zamyka polonczenie\n  ip: %s\n  port: %d\n\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));  
+                    close(current_sd);
                     client_sd[i] = 0;  
                 }  
                 else 
                 {  
+                    //inna aktynosc
                     buff[retval] = '\0';  
-                    printf("sd: %d\n",current_sd);
+
+                    // rozeslij wiadomosc do wszystkich klientow...
                     for (int j = 0; j < MAX_CLIENTS; j++)  
                     {  
-                        if( client_sd[j] == 0 )  
+                        if( client_sd[j] == 0 )  // ...oprocz wysylajacego.
                         {  
                             break;  
                         }
                         else if(client_sd[j]!=current_sd)
                         {
-                            //write(j,buff,strlen(buff));
-                            printf("sd: %d\n",j);
                             send(client_sd[j], buff, strlen(buff), 0);  
                         }  
                     }  
-                    //broadcast_msg(current_sd,buff,0);
                 }
             }  
         }  
