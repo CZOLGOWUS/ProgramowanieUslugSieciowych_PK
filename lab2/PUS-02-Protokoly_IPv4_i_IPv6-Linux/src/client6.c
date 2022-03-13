6f7a57129da2711ba847d65c9b00e3c542b9407f
CZOLGOWUS <jkolodziej58@outlook.com>
2022-03-13 18:36:28 +0100

    zad client ip6 and server ip4

--- lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/client6.c
+++ lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/client6.c
@@ -1,4 +1,81 @@
-// This is dummy main() to force make to compile other files
-int main(){
-    return -1;
+/*
+ * Uruchamianie:        $ ./client1 <adres IP> <numer portu>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> /* socket() */
+#include <netinet/in.h> /* struct sockaddr_in */
+#include <arpa/inet.h>  /* inet_pton() */
+#include <unistd.h>     /* close() */
+#include <string.h>
+#include <netdb.h>
+#include <net/if.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+
+    int sockfd;
+    int retval;
+    struct sockaddr_in6 remote_addr;
+    socklen_t addr_len;
+    char buff[256];
+
+    struct hostent *host;
+
+    if (argc != 4)
+    {
+        fprintf(stderr, "Invocation: %s <IPv6 ADDRESS> <PORT> <INTERFACE_NAME>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    host = gethostbyname(argv[1]);
+
+    // Utworzenie gniazda dla protokolu TCP:
+    sockfd = socket(PF_INET6, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    // Budowa struktury adresowej dla adresu serwera
+    memset(&remote_addr, 0, sizeof(remote_addr));
+    remote_addr.sin6_family = AF_INET6;
+    retval = inet_pton(AF_INET6, argv[1], &remote_addr.sin6_addr);
+    if (retval == 0)
+    {
+        fprintf(stderr, "inet_pton(): invalid network address!\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (retval == -1)
+    {
+        perror("inet_pton()");
+        exit(EXIT_FAILURE);
+    }
+
+    remote_addr.sin6_port = htons(atoi(argv[2]));
+    // dodanie interfejsu do struktury
+    remote_addr.sin6_scope_id = htons(if_nametoindex(argv[3]));
+    addr_len = sizeof(remote_addr);
+
+    // Rozpoczencie poloczenia:
+    if (connect(sockfd, (const struct sockaddr *)&remote_addr, addr_len) == -1)
+    {
+        perror("connect()");
+        exit(EXIT_FAILURE);
+    }
+
+    // Odebranie danych:
+    memset(buff, 0, 256);
+    retval = read(sockfd, buff, sizeof(buff));
+    printf("Received server response: %s\n", buff);
+
+    // Zamkniecie clienta:
+    printf("Closing socket (sending FIN to server)...\n");
+    close(sockfd);
+    printf("Terminating application. After receiving FIN from server, "
+           "TCP connection will go into TIME_WAIT state.\n");
+    exit(EXIT_SUCCESS);
 }
--- lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/server4.c
+++ lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/server4.c
@@ -1,4 +1,131 @@
-// This is dummy main() to force make to compile other files
-int main(){
-    return -1;
+/*
+ * Uruchamianie:        $ ./server1 <numer portu>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> /* socket() */
+#include <netinet/in.h> /* struct sockaddr_in */
+#include <arpa/inet.h>  /* inet_ntop() */
+#include <unistd.h>     /* close() */
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+
+void printCurrentTime()
+{
+    time_t now;
+    struct tm *tm;
+
+    now = time(0);
+    if ((tm = localtime(&now)) == NULL)
+    {
+        printf("Error extracting time stuff\n");
+        return;
+    }
+
+    printf("[ %02d:%02d:%02d ] ",
+           tm->tm_hour, tm->tm_min, tm->tm_sec);
+}
+
+int main(int argc, char **argv)
+{
+
+    /* Deskryptory dla gniazda nasluchujacego i polaczonego: */
+    int server_sd, client_sd;
+
+    int retval;
+
+    /* Gniazdowe struktury adresowe (dla klienta i serwera): */
+    struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+
+    /* Rozmiar struktur w bajtach: */
+    socklen_t client_addr_len, server_addr_len;
+
+    /* Bufor danych: */
+    char buff[256];
+
+    /* Bufor dla adresu IP klienta w postaci kropkowo-dziesietnej: */
+    char addr_buff[256];
+
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invocation: %s <PORT>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Utworzenie gniazda dla protokolu TCP:
+    server_sd = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_sd == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    // Tworzenie struktury adresowej serwera:
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+    server_addr_len = sizeof(struct sockaddr_in);
+
+    // Przygotowanie gniazda servera
+    if (bind(server_sd, (struct sockaddr *)&server_addr, server_addr_len) == -1)
+    {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_sd, 2) == -1)
+    {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        // oczekiwanie na polocznie przychodzace
+        printCurrentTime();
+        printf("Server is listening for incoming connection...\n");
+
+        client_addr_len = sizeof(client_addr);
+        client_sd = accept(server_sd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sd == -1)
+        {
+            perror("accept()");
+            exit(EXIT_FAILURE);
+        }
+
+        printCurrentTime();
+        printf(
+            "TCP connection accepted from %s port %d\n",
+            inet_ntop(AF_INET, &client_addr.sin_addr, addr_buff, sizeof(addr_buff)),
+            ntohs(server_addr.sin_port));
+
+        // Wyslanie odpowiedzi:
+        printCurrentTime();
+        printf("Sending message...\n");
+
+        retval = sendto(
+            client_sd,
+            "Laboratorium PUS",
+            17,
+            0,
+            (struct sockaddr *)&client_addr,
+            client_addr_len);
+
+        printCurrentTime();
+        printf("Closing connected socket (sending FIN to client)...\n");
+        close(client_sd);
+    }
+
+    printCurrentTime();
+    printf("Closing listening socket and terminating server...\n");
+    close(server_sd);
+
+    exit(EXIT_SUCCESS);
 }
