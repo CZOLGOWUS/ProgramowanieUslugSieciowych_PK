8ad46207cd5b580e1d345d9dfd63f6c33e2559aa
CKTA00 <przekozuch@gmail.com>
2022-03-19 15:14:31 +0100

    Working universal client. Wrong output

--- lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/client.c
+++ lab2/PUS-02-Protokoly_IPv4_i_IPv6-Linux/src/client.c
@@ -20,11 +20,13 @@ int main(int argc, char** argv) {
 
     int sockfd;
     int retval;                 
-    struct sockaddr_in remote_addr; // deprecated: replace with server_addr
-    struct sockaddr_storage server_addr;
-    struct addrinfo* universal_addr;
+    struct sockaddr universal_server_addr;
+    struct sockaddr_storage universal_server_addr_storage;
+    struct addrinfo* universal_addr_info;
     socklen_t addr_len;
     char buff[256];
+    char serv_buff[256];
+    char host_buff[256];
 
     if (argc != 3) {
         fprintf(stderr, "Invocation: %s <IPv4 or IPv6 ADDRESS> <PORT>\n", argv[0]);
@@ -32,56 +34,89 @@ int main(int argc, char** argv) {
     }
 
     // Zidentyfikowanie rodzaju adresu podanego przez uzytkownika
-    if(getaddrinfo(argv[1], NULL, NULL, &universal_addr) != 0)
+    if(getaddrinfo(argv[1], NULL, NULL, &universal_addr_info) != 0)
     {
         perror("getaddrinfo()");
         exit(EXIT_FAILURE);
     }
 
-    if(universal_addr->ai_family == AF_INET)
+    if(universal_addr_info->ai_family == AF_INET)
     {
         printf("Given address is in IPv4 family\n");
+
+        struct sockaddr_in ipv4_addr;
+        memset(&ipv4_addr, 0, sizeof(ipv4_addr));
+        ipv4_addr.sin_family = AF_INET;
+        retval = inet_pton(AF_INET, argv[1], &ipv4_addr.sin_addr);
+        if (retval == 0) {
+            fprintf(stderr, "inet_pton(): invalid network address!\n");
+            exit(EXIT_FAILURE);
+        } else if (retval == -1) {
+            perror("inet_pton()");
+            exit(EXIT_FAILURE);
+        }
+        ipv4_addr.sin_port = htons(atoi(argv[2]));
+        addr_len = sizeof(ipv4_addr);
+
+        memcpy (&universal_server_addr_storage, &ipv4_addr, sizeof (ipv4_addr));
     }
-    else if(universal_addr->ai_family == AF_INET6)
+    else if(universal_addr_info->ai_family == AF_INET6)
     {
         printf("Given address is in IPv6 family\n");
+
+        struct sockaddr_in6 ipv6_addr;
+        memset(&ipv6_addr, 0, sizeof(ipv6_addr));
+        ipv6_addr.sin6_family = AF_INET6;
+        retval = inet_pton(AF_INET6, argv[1], &ipv6_addr.sin6_addr);
+        if (retval == 0)
+        {
+            fprintf(stderr, "inet_pton(): invalid network address!\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (retval == -1)
+        {
+            perror("inet_pton()");
+            exit(EXIT_FAILURE);
+        }
+
+        memcpy (&universal_server_addr_storage, &ipv6_addr, sizeof (ipv6_addr));
     }
     else
     {
         printf("Not an IP address!\n");
-    }
-    exit(0);
-
-    // Budowa struktury adresowej dla adresu serwera
-
-    //memset(&remote_addr, 0, sizeof(remote_addr));
-    memset(&server_addr, 0, sizeof(server_addr));
-    //remote_addr.sin_family = AF_INET;
-    ///server_addr.ss_family =
-    retval = inet_pton(AF_INET, argv[1], &remote_addr.sin_addr);
-    if (retval == 0) {
-        fprintf(stderr, "inet_pton(): invalid network address!\n");
-        exit(EXIT_FAILURE);
-    } else if (retval == -1) {
-        perror("inet_pton()");
         exit(EXIT_FAILURE);
     }
-    remote_addr.sin_port = htons(atoi(argv[2]));
-    addr_len = sizeof(remote_addr);
-
-    // Utworzenie gniazda dla protokolu TCP:
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    
+    // Utworzenie gniazda odpowiedniego typu
+    sockfd = socket(universal_addr_info->ai_family, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("socket()");
         exit(EXIT_FAILURE);
     }
 
-
     // Rozpoczencie poloczenia: 
-    if (connect(sockfd, (const struct sockaddr*) &remote_addr, addr_len) == -1) {
+    if (connect(sockfd, (const struct sockaddr *)&universal_server_addr_storage, addr_len) == -1) {
         perror("connect()");
         exit(EXIT_FAILURE);
     }
+
+    //Wypisanie danych
+    memset(serv_buff, 0, 256);
+    memset(host_buff, 0, 256);
+    socklen_t storage_size;
+    getsockname(sockfd, (struct sockaddr*)&universal_server_addr_storage, &storage_size);
+    getnameinfo(
+        (struct sockaddr*)&universal_server_addr_storage, storage_size,
+        host_buff, 256,
+        serv_buff, 256,
+        NI_NUMERICHOST|NI_NUMERICSERV
+    );
+
+    printf(
+        "Host IP: %s\nServer IP: %s\n",
+        host_buff,
+        serv_buff
+    );
 
     // Odebranie danych:
     memset(buff, 0, 256);
