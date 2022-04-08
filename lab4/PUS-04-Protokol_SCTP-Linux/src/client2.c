380d531814f8e091dc8d38401373b0d6a3daa97b
CKTA00 <przekozuch@gmail.com>
2022-04-08 14:27:00 +0200

    Implemented task 2: client and server

---
+++ lab4/PUS-04-Protokol_SCTP-Linux/src/client2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/sctp.h>
+
+#define BUFF_SIZE 256
+
+int main(int argc, char** argv) {
+
+    int                     sockfd;
+    int                     retval;
+    struct addrinfo hints, *result;
+    char                    buff[BUFF_SIZE];
+
+    // unsigned char scpt_options[0] = {
+    // };
+
+    struct sctp_initmsg sctp_options;
+    struct sctp_status sctp_sts;
+
+    if (argc != 3) {
+        fprintf(stderr, "Invocation: %s <IP ADDRESS> <PORT NUMBER>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family         = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype       = SOCK_STREAM;
+    hints.ai_flags          = 0;
+    hints.ai_protocol       = 0;
+
+    retval = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (retval != 0) {
+        fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+
+    if (result == NULL) {
+        fprintf(stderr, "Could not connect!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sockfd = socket(result->ai_family, result->ai_socktype, IPPROTO_SCTP);
+    if (sockfd == -1) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&sctp_options, 0, sizeof(struct sctp_initmsg));
+    sctp_options.sinit_num_ostreams = 3;
+    sctp_options.sinit_max_instreams = 4;
+    sctp_options.sinit_max_attempts = 5;
+    //sctp_options.sinit_max_init_timeo = ?;
+    retval = setsockopt(sockfd, IPPROTO_SCTP, SCTP_INITMSG, (void*)&sctp_options, sizeof(struct sctp_initmsg));
+    if (retval != 0) {
+        fprintf(stderr, "setsockopt()\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sockfd, result->ai_addr, result->ai_addrlen) == -1) {
+        perror("connect()");
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);
+
+    getsockopt(sockfd, IPPROTO_SCTP, SCTP_STATUS, &sctp_sts, NULL);
+    printf("ID: %d\n",sctp_sts.sstat_assoc_id);
+    printf("Association state: %d\n",sctp_sts.sstat_state);
+    printf("Output sterams: %d\n",sctp_sts.sstat_outstrms);
+    printf("Input sterams: %d\n",sctp_sts.sstat_instrms);
+
+    retval = sctp_recvmsg(sockfd,buff, BUFF_SIZE,NULL, NULL, NULL, NULL);
+    if (retval == -1) {
+        perror("sctp_recvmsg()");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received 1: %s\n",buff);
+
+    retval = sctp_recvmsg(sockfd,buff, BUFF_SIZE,NULL, NULL, NULL, NULL);
+    if (retval == -1) {
+        perror("sctp_recvmsg()");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Received 2: %s\n",buff);
+    
+    close(sockfd);
+    exit(EXIT_SUCCESS);
+}
---
+++ lab4/PUS-04-Protokol_SCTP-Linux/src/server2.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/sctp.h>
+#include <time.h>
+
+#define BUFF_SIZE 256
+
+int main(int argc, char** argv) {
+
+    int                     listenfd, acceptedfd;
+    int                     retval;
+    struct sockaddr_in      servaddr;
+    char                    buffer[BUFF_SIZE];
+
+    struct sctp_initmsg sctp_options;
+    struct sctp_status sctp_sts;
+
+    if (argc != 2) {
+        fprintf(stderr, "Invocation: %s <PORT NUMBER>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    if (listenfd == -1) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&sctp_options, 0, sizeof(struct sctp_initmsg));
+    sctp_options.sinit_num_ostreams = 2;
+    sctp_options.sinit_max_instreams = 2;
+    sctp_options.sinit_max_attempts = 5;
+    //sctp_options.sinit_max_init_timeo = ?;
+    retval = setsockopt(listenfd, IPPROTO_SCTP, SCTP_INITMSG, (void*)&sctp_options, sizeof(struct sctp_initmsg));
+    if (retval != 0) {
+        fprintf(stderr, "setsockopt()\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family             =       AF_INET;
+    servaddr.sin_port               =       htons(atoi(argv[1]));
+    servaddr.sin_addr.s_addr        =       htonl(INADDR_ANY);
+
+    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1) {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(listenfd, 5) == -1) {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+
+        acceptedfd = accept(listenfd, NULL, 0);
+        if (acceptedfd == -1) {
+            close(acceptedfd);
+            perror("accept()");
+            exit(EXIT_FAILURE);
+        }
+
+        time_t current_time = time(NULL);
+        struct tm* curr_tm = localtime(&current_time);
+        strftime(buffer, BUFF_SIZE, "%x", curr_tm);
+        printf("DATE SENT: %s\n",buffer);
+
+        retval = sctp_sendmsg(acceptedfd,(void*)buffer,BUFF_SIZE,NULL,0,0,0,0,1000,0);
+        if(retval==-1)
+        {
+            close(listenfd);
+            close(acceptedfd);
+            perror("sctp_sendmsg()");
+            exit(EXIT_FAILURE);
+        }
+
+        strftime(buffer, BUFF_SIZE, "%X", curr_tm);
+        printf("TIME SENT: %s\n",buffer);
+
+        retval = sctp_sendmsg(acceptedfd,(void*)buffer,BUFF_SIZE,NULL,0,0,0,1,1000,0);
+        if(retval==-1)
+        {
+            close(listenfd);
+            close(acceptedfd);
+            perror("sctp_sendmsg()");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+}
