3fa7f8b44c0ee4bba487102b0163fe0bfb440c5a
CZOLGOWUS <jkolodziej58@outlook.com>
2022-04-09 00:25:38 +0200

    lab 4 zad3 - testing

---
+++ lab4/PUS-04-Protokol_SCTP-Linux/src/client3.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/sctp.h>
+
+#define BUFF_SIZE 256
+
+#define IN_STREAMS 3
+#define OUT_STREAMS 3
+#define ATTEMPTS 4
+
+typedef struct sctp_sndrcvinfo SndInfo;
+void print_client_info( SndInfo* info)
+{
+	printf("Stream = %d\n",info->sinfo_stream);
+	printf("Id = %d\n",info->sinfo_assoc_id);
+	printf("SSN = %d\n",info->sinfo_ssn);
+	printf("TSN = %d\n",info->sinfo_tsn);
+}
+
+
+int main(int argc, char** argv) 
+{
+
+    int                     sockfd;
+    int                     retval, bytes;
+    char                    *retptr;
+
+
+    struct addrinfo hints, *result;
+
+	struct sctp_initmsg init_msg;
+	struct sctp_status status;
+	struct sctp_sndrcvinfo snd_info;
+	struct sockaddr from_addr;
+
+
+    socklen_t from_len = sizeof(from_addr);
+    socklen_t opt_len = sizeof(struct sctp_status);
+
+
+    char                    buff[BUFF_SIZE] = "initial";
+	unsigned short stream_num = 0;
+	
+
+
+    if (argc != 3)
+	{
+        fprintf(stderr, "Invocation: %s <IP ADDRESS> <PORT NUMBER>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family         = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype       = SOCK_STREAM;
+    hints.ai_flags          = 0;
+    hints.ai_protocol       = 0;
+
+    retval = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (retval != 0) 
+	{
+        fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+
+    if (result == NULL) 
+	{
+        fprintf(stderr, "Could not connect!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sockfd = socket(result->ai_family, result->ai_socktype, IPPROTO_SCTP);
+    if (sockfd == -1) 
+	{
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+	memset(&init_msg, 0, sizeof(struct sctp_initmsg));
+
+    init_msg.sinit_max_instreams = IN_STREAMS;
+    init_msg.sinit_num_ostreams = OUT_STREAMS;
+    init_msg.sinit_max_attempts = ATTEMPTS;
+
+	if ( (retval = setsockopt(sockfd, IPPROTO_SCTP, SCTP_INITMSG, &init_msg, sizeof(struct sctp_initmsg))) == -1) 
+	{
+		perror("setsockopt()");
+		exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);
+
+
+    for (;;) 
+	{
+		retval = sctp_sendmsg(sockfd,(void*)buff, BUFF_SIZE,result->ai_addr, result->ai_addrlen,0,0,stream_num,10,0 );
+		if(retval == -1)
+		{
+			perror("sctp_sendmsg()");
+            exit(EXIT_FAILURE);
+		}
+
+		bytes = sctp_recvmsg(sockfd, buff, sizeof(buff), result->ai_addr, &(result->ai_addrlen) , &snd_info, NULL);
+		if(bytes == -1)
+		{
+			perror("sctp_recvmsg()");
+            exit(EXIT_FAILURE);
+		}
+
+		print_client_info(&snd_info);
+		printf("received msg = \'%s\'\n",buff);
+
+		memset(buff,0,BUFF_SIZE);
+
+    }
+
+    close(sockfd);
+    return 0;
+}
---
+++ lab4/PUS-04-Protokol_SCTP-Linux/src/server3.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/sctp.h>
+
+#define BUFF_SIZE 256
+
+#define IN_STREAMS 3
+#define OUT_STREAMS 3
+#define ATTEMPTS 4
+
+
+typedef struct sctp_sndrcvinfo SndInfo;
+void print_server_info_recv(int stream_id, SndInfo* info)
+{
+	printf("Stream = %d\n",stream_id);
+	printf("Id = %d\n",info->sinfo_assoc_id);
+	printf("SSN = %d\n",info->sinfo_ssn);
+	printf("TSN = %d\n",info->sinfo_tsn);
+}
+
+int main(int argc, char** argv)
+{
+	int option_selected = -1;
+	int current_stream_id = 0;
+	int to_send_stream_id = 0;
+
+	int                     listenfd;
+    int                     retval, bytes;
+    struct sockaddr_in      server_addr;
+    struct sockaddr dest_addr;
+	socklen_t dest_addr_len = sizeof(dest_addr);
+	
+	struct sctp_initmsg init_msg;
+	struct sctp_sndrcvinfo send_receive_info;
+
+	char buff[BUFF_SIZE] = "Sample Data from server";
+	
+    if (argc != 3) 
+	{
+        fprintf(stderr, "Invocation: %s <PORT NUMBER> <OPTION>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+	option_selected = atoi(argv[2]);
+	if(option_selected != 0 && option_selected != 1)
+	{
+		printf("option can be \'0\' or \'1\'");
+		exit(EXIT_FAILURE);
+	}
+
+    listenfd = socket(PF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+    if (listenfd == -1) 
+	{
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+
+	memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family             =       AF_INET;
+    server_addr.sin_port               =       htons(atoi(argv[1]));
+    server_addr.sin_addr.s_addr        =       htonl(INADDR_ANY);
+
+    memset(&init_msg, 0, sizeof(struct sctp_initmsg));
+    init_msg.sinit_max_instreams = IN_STREAMS;
+    init_msg.sinit_num_ostreams = OUT_STREAMS;
+    init_msg.sinit_max_attempts = ATTEMPTS;
+
+    retval = setsockopt(listenfd, IPPROTO_SCTP, SCTP_INITMSG, &init_msg, sizeof(struct sctp_initmsg));
+
+    if (retval == -1) 
+	{
+        perror("setsockopt()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) 
+	{
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(listenfd, 10) == -1) 
+	{
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+
+	for(;;)
+    {
+        if (option_selected) 
+		{
+            current_stream_id = to_send_stream_id;
+
+            if( (to_send_stream_id + 1) == OUT_STREAMS)
+                to_send_stream_id = -1;
+
+            ++to_send_stream_id;
+        }
+
+		bytes = sctp_recvmsg(listenfd, buff, sizeof(buff), &dest_addr, &dest_addr_len, &send_receive_info, NULL);
+        if (bytes == -1) 
+		{
+            perror("recv()");
+            exit(EXIT_FAILURE);
+        }
+
+        print_server_info_recv(current_stream_id,&send_receive_info);
+		printf("received msg = \'%s\'\n",buff);
+
+		retval = sctp_sendmsg(listenfd, (void*)buff, BUFF_SIZE, &dest_addr, dest_addr_len,0,0,current_stream_id,10,0);
+		if(retval == -1)
+		{
+			perror("sctp_sendmsg()");
+			exit(EXIT_FAILURE);
+		}
+
+		memset(buff,0,BUFF_SIZE);
+
+	}
+
+	close(listenfd);
+
+}
