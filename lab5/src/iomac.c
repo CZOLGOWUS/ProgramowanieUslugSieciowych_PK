e91586497e8b14e686f059a0a9694395b3cc3ef3
CZOLGOWUS <jkolodziej58@outlook.com>
2022-04-15 00:12:16 +0200

    lab 5 zad 4 fix

--- lab5/src/iomac.c
+++ lab5/src/iomac.c
@@ -39,44 +39,46 @@ void print_MAC_MTU(int socket_descriptor, struct ifreq* if_info)
 int main(int argc, char** argv) {
 
     int                     sockfd, retval;
-    struct ifreq if_retval;
+    struct ifreq if_info;
 
 
-    if (argc != 4) {
+    if (argc != 4) 
+    {
         fprintf(stderr, "Invocation: %s <IPv4 ADDRESS> <MAC ADDRESS> <NEW_MTU>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
+    if (sockfd == -1) 
+    {
         perror("socket()");
         exit(EXIT_FAILURE);
     }
 
-    strcpy(if_retval.ifr_name, argv[1]);
+    strcpy(if_info.ifr_name, argv[1]);
 
-    //get mac and MTU info
-    if ((retval = ioctl(sockfd, SIOCGIFHWADDR, if_retval)) == -1) 
+    //get current mac and MTU info
+    if ((retval = ioctl(sockfd, SIOCGIFHWADDR, &if_info)) == -1) 
     {
-        perror("ioctl()");
+        perror("ioctl() first");
         exit(EXIT_FAILURE);
     }
 
     printf("before chang\n");
-    print_MAC_MTU(sockfd, &if_retval);
+    print_MAC_MTU(sockfd, &if_info);
     printf("\n\n");
 
     /* Adres MAC: */
-    if_retval.ifr_hwaddr.sa_family = ARPHRD_ETHER;
+    if_info.ifr_hwaddr.sa_family = ARPHRD_ETHER;
 
     retval = sscanf(
                  argv[2], "%2x:%2x:%2x:%2x:%2x:%2x",
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[0],
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[1],
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[2],
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[3],
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[4],
-                 (unsigned int*)&if_retval.ifr_hwaddr.sa_data[5]
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[0],
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[1],
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[2],
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[3],
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[4],
+                 (unsigned int*)&if_info.ifr_hwaddr.sa_data[5]
              );
 
     if (retval != 6) 
@@ -85,26 +87,26 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    if ((retval = ioctl(sockfd, SIOCSIFHWADDR, &if_retval)) == -1)
+    if ((retval = ioctl(sockfd, SIOCSIFHWADDR, &if_info)) == -1)
     {
-        perror("ioctl()");
+        perror("ioctl() second");
         exit(EXIT_FAILURE);
     }
 
-    if_retval.ifr_mtu = atoi(argv[3]);
+    if_info.ifr_mtu = atoi(argv[3]);
 
-    retval = ioctl(sockfd, SIOCSIFMTU, &if_retval);
+    retval = ioctl(sockfd, SIOCSIFMTU, &if_info);
 
     
-    //get mac and MTU info
-    if ((retval = ioctl(sockfd, SIOCGIFHWADDR, if_retval)) == -1) 
+    //get current mac and MTU info
+    if ((retval = ioctl(sockfd, SIOCGIFHWADDR, if_info)) == -1) 
     {
-        perror("ioctl()");
+        perror("ioctl() third");
         exit(EXIT_FAILURE);
     }
     
     printf("before chang\n");
-    print_MAC_MTU(sockfd, &if_retval);
+    print_MAC_MTU(sockfd, &if_info);
     printf("\n\n");
 
     close(sockfd);
