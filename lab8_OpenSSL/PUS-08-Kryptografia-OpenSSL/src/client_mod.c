15a26b6552cc74664a8536b331afde51cc3bcf31
CKTA00 <przekozuch@gmail.com>
2022-05-16 13:32:19 +0200

    Add HMAC verification

--- lab8_OpenSSL/PUS-08-Kryptografia-OpenSSL/src/client_mod.c
+++ lab8_OpenSSL/PUS-08-Kryptografia-OpenSSL/src/client_mod.c
@@ -7,6 +7,12 @@
 #include <string.h>
 #include <errno.h>
 
+#include <openssl/hmac.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+
+#define MD5_DIGEST_SIZE 16
+
 int main(int argc, char** argv) {
 
     int             sockfd;                 /* Desktryptor gniazda. */
@@ -14,13 +20,73 @@ int main(int argc, char** argv) {
     struct          sockaddr_in remote_addr;/* Gniazdowa struktura adresowa. */
     socklen_t       addr_len;               /* Rozmiar struktury w bajtach. */
 
-    char            buff[256] = "Laboratorium PUS.";
+        /* Bufor do wyslania przez UDP: */
+    char buff[256+MD5_DIGEST_SIZE];
+        /* Wiadomosc: */
+    char message[256] = "Laboratorium PUS.";
+        /* Skrot wiadomosci: */
+    char result[MD5_DIGEST_SIZE];
+
+        /* Klucz: */
+    char key[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                           0x00,0x01,0x02,0x03,0x04,0x05
+                          };
+
+    /* Rozmiar tekstu i szyfrogramu: */
+    unsigned int message_len, result_len;
+
+    /* Kontekst: */
+    HMAC_CTX *ctx;
 
 
     if (argc != 3) {
         fprintf(stderr, "Invocation: %s <IPv4 ADDRESS> <PORT>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    ////////////////////////////////// HMAC
+    message_len = strlen(message);
+
+    /* Alokacja pamieci dla kontekstu: */
+    ctx = HMAC_CTX_new();
+
+    /* Inicjalizacja kontekstu: */
+    //HMAC_CTX_init(ctx);
+    HMAC_CTX_reset(ctx); // in newer version
+
+    /* Konfiguracja kontekstu: */
+    retval = HMAC_Init_ex(ctx, key, sizeof(key), EVP_md5(), NULL);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Obliczenie kodu HMAC: */
+    retval = HMAC_Update(ctx, message, message_len);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /*Zapisanie kodu HMAC w buforze 'result': */
+    retval = HMAC_Final(ctx, result, &result_len);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Usuwa wszystkie informacje z kontekstu i zwalnia pamiec zwiazana
+     * z kontekstem:
+     */
+    //HMAC_CTX_cleanup(ctx);
+    HMAC_CTX_free(ctx);  //in newer version
+
+
+    ////////////////////////////////// UDP
+
+    strcpy(buff, result);
+    strcpy(buff+MD5_DIGEST_SIZE, message);
 
     /* Utworzenie gniazda dla protokolu UDP: */
     sockfd = socket(PF_INET, SOCK_DGRAM, 0);
--- lab8_OpenSSL/PUS-08-Kryptografia-OpenSSL/src/server_mod.c
+++ lab8_OpenSSL/PUS-08-Kryptografia-OpenSSL/src/server_mod.c
@@ -7,6 +7,12 @@
 #include <string.h>
 #include <errno.h>
 
+#include <openssl/hmac.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+
+#define MD5_DIGEST_SIZE 16
+
 int main(int argc, char** argv) {
 
     int             sockfd; /* Deskryptor gniazda. */
@@ -18,11 +24,27 @@ int main(int argc, char** argv) {
     /* Rozmiar struktur w bajtach: */
     socklen_t       client_addr_len, server_addr_len;
 
-    /* Bufor wykorzystywany przez recvfrom(): */
-    char            buff[256];
-
     /* Bufor dla adresu IP klienta w postaci kropkowo-dziesietnej: */
     char            addr_buff[256];
+
+            /* Bufor odebrania przez UDP: */
+    char buff[256+MD5_DIGEST_SIZE];
+        /* Wiadomosc: */
+    char message[256];
+        /* Skrot wiadomosci: */
+    char result[MD5_DIGEST_SIZE];
+
+        /* Klucz: */
+    char key[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,
+                           0x00,0x01,0x02,0x03,0x04,0x05
+                          };
+
+
+    /* Rozmiar tekstu i szyfrogramu: */
+    unsigned int message_len, result_len;
+
+    /* Kontekst: */
+    HMAC_CTX *ctx;
 
 
     if (argc != 2) {
@@ -71,12 +93,64 @@ int main(int argc, char** argv) {
     }
 
     fprintf(stdout, "UDP datagram received from %s:%d.\n",
-            inet_ntop(AF_INET, &client_addr.sin_addr, addr_buff, sizeof(addr_buff)),
-            ntohs(client_addr.sin_port)
-           );
+        inet_ntop(AF_INET, &client_addr.sin_addr, addr_buff, sizeof(addr_buff)),
+        ntohs(client_addr.sin_port)
+        );
+
+    strcpy(message, buff+MD5_DIGEST_SIZE);
+    message_len = strlen(message);
+
+    ////////////////////////////////// HMAC
+    
+
+    /* Alokacja pamieci dla kontekstu: */
+    ctx = HMAC_CTX_new();
+
+    /* Inicjalizacja kontekstu: */
+    //HMAC_CTX_init(ctx);
+    HMAC_CTX_reset(ctx); // in newer version
+
+    /* Konfiguracja kontekstu: */
+    retval = HMAC_Init_ex(ctx, key, sizeof(key), EVP_md5(), NULL);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Obliczenie kodu HMAC: */
+    retval = HMAC_Update(ctx, message, message_len);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /*Zapisanie kodu HMAC w buforze 'result': */
+    retval = HMAC_Final(ctx, result, &result_len);
+    if (!retval) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Usuwa wszystkie informacje z kontekstu i zwalnia pamiec zwiazana
+     * z kontekstem:
+     */
+    //HMAC_CTX_cleanup(ctx);
+    HMAC_CTX_free(ctx);  //in newer version
+
+    // Weryfikacja HMAC:
+    for(int i = 0; i<MD5_DIGEST_SIZE; i++)
+    {
+        if(buff[i] != result[i])
+        {
+            printf("Verification failed: HMAC digest do not match!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("HMAC verification successful.\n");
 
     fprintf(stdout, "Message: ");
-    fwrite(buff, sizeof(char), retval, stdout);
+    fwrite(message, message_len, retval, stdout);
     fprintf(stdout, "\n");
 
     close(sockfd);
